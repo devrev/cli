5a2089806738eacc5dbf06967bc57f8eb40ed9a3
SathishKumarHS <i-Sathish.KumarHS@devrev.ai>
2023-04-03 18:57:47 +0530

    Version v0.3.2¸ added

--- devrev.rb
+++ devrev.rb
@@ -1,12 +1,12 @@
 class Devrev < Formula
   desc "devrev-cli"
   homepage "https://devrev.ai"
-  version "1.1.1"
+  version "0.3.2¸"
 
   on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/devrev/brew-devrev-cli/releases/download/v1.1.1/devrev_1.1.1_Darwin_arm64.tar.gz"
-        sha256 "91917607ad04ca64b124fb0b53811de15b83c0a6492862e3c95da9d0676618f1"
+        url "https://github.com/devrev/brew-devrev-cli/releases/download/v0.3.2¸/devrev_0.3.2¸_Darwin_arm64.tar.gz"
+        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         def install
           bin.install "devrev"
         end
@@ -16,8 +16,8 @@ gateway version 2e39352305a8596ba6a8e78707be80d7e4abcde7" , shell_output("devrev
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/devrev/brew-devrev-cli/releases/download/v1.1.1/devrev_1.1.1_Darwin_x86_64.tar.gz"
-        sha256 "b8bb6a41fbf38ae723d6aa236e0166b5613a4db75a4bad87132e9f19cf0517e8"
+        url "https://github.com/devrev/brew-devrev-cli/releases/download/v0.3.2¸/devrev_0.3.2¸_Darwin_x86_64.tar.gz"
+        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         def install
           bin.install "devrev"
         end
@@ -29,8 +29,8 @@ gateway version 2e39352305a8596ba6a8e78707be80d7e4abcde7" , shell_output("devrev
     end
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/devrev/brew-devrev-cli/releases/download/v1.1.1/devrev_1.1.1_Linux_arm64.tar.gz"
-        sha256 "63e54f288edb6c05a6fc4e7e8174a4697c7c70b431afc9b9fe64fc0db953ad2b"
+        url "https://github.com/devrev/brew-devrev-cli/releases/download/v0.3.2¸/devrev_0.3.2¸_Linux_arm64.tar.gz"
+        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         def install
           bin.install "devrev"
         end
@@ -40,8 +40,8 @@ gateway version 2e39352305a8596ba6a8e78707be80d7e4abcde7" , shell_output("devrev
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/devrev/brew-devrev-cli/releases/download/v1.1.1/devrev_1.1.1_Linux_x86_64.tar.gz"
-        sha256 "d069bfd102a3d6fddf77a786851950055c5e737f365004c5751cb3c303b19e79"
+        url "https://github.com/devrev/brew-devrev-cli/releases/download/v0.3.2¸/devrev_0.3.2¸_Linux_x86_64.tar.gz"
+        sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         def install
           bin.install "devrev"
         end
